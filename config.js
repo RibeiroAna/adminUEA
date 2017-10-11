112f15221eefab8341e6e87e4321ba01817e4012
Ana Ribeiro <aninhacostaribeiro@gmail.com>
2017-10-10 21:17:23 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 app.value('config', {
-   "api_url": "http://localhost:3000",
+   "api_url": "https://api.uea.splab.ufcg.edu.br/",
    "idAdministranto": 1,
    "idJunaAdministranto": 7,
    "idBazaMembreco": 7
