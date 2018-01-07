2dc25176129d44112ef0c0d6524da84c59a9dd75
Jefferson Carlos Benedito <jefiim.ad@gmail.com>
2018-01-06 23:06:21 -0200

    add select box and checkbox options to printejo page

--- components/membroj/printejoCtrl.js
+++ components/membroj/printejoCtrl.js
@@ -1,3 +1,15 @@
 app.controller('printejoCtrl', function ($scope) {
+	console.log("eyeyyee");
 
+	$scope.checkboxFields = [{'name': 'Tuta nomo'}, 
+							{'name': 'UEA-kodo'}, 
+							{'name': 'Adreso'}, 
+							{'name': 'Postkodo'},
+							{'name': 'Lando - FR'},
+							{'name': 'Lando - EO'},
+							{'name': 'Lando - NL'},
+							{'name': 'Lando - EN'},
+							{'name': 'Telefono'},
+							{'name': 'Retadreso'}
+							];
 });
