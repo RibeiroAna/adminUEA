2b084ae6403a0d02c1219222c26c77a301d0d1f3
Jefferson Carlos Benedito <jefiim.ad@gmail.com>
2018-01-18 18:17:22 -0300

    Generate pdf file

--- components/membroj/printejoCtrl.js
+++ components/membroj/printejoCtrl.js
@@ -1,5 +1,4 @@
 app.controller('printejoCtrl', function ($scope) {
-	console.log("eyeyyee");
 
 	$scope.checkboxFields = [{'name': 'Tuta nomo'}, 
 							{'name': 'UEA-kodo'}, 
@@ -12,4 +11,26 @@ app.controller('printejoCtrl', function ($scope) {
 							{'name': 'Telefono'},
 							{'name': 'Retadreso'}
 							];
+
+	$scope.grupoj = [{nomo: 'Jefferson benedito', adreso: 'Rua 15 de Janeiro', postkodo: '58345-000', lando: 'Brazil'},
+					{nomo: 'Jhon Jhon', adreso: 'Rua 15 de Janeiro', postkodo: '58345-000', lando: 'Brazil'},
+					{nomo: 'Jhon Jhon', adreso: 'Rua 15 de Janeiro', postkodo: '58345-000', lando: 'Brazil'},
+		{nomo: 'Jhon Jhon', adreso: 'Rua 15 de Janeiro', postkodo: '58345-000', lando: 'Brazil'}];
+
+
+	$scope.teste = function () {
+		html2canvas(document.getElementById('exportthis')).then(function(canvas) {
+			var data = canvas.toDataURL();
+			var docDefinition = {
+				content: [{
+					image: data,
+					width: 500,
+				}]
+			};
+			pdfMake.createPdf(docDefinition).download("Score_Details.pdf");
+		});
+	}
+
+
+
 });
