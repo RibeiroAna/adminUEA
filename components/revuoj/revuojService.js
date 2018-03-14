3aeedac4ca0d836630c0f8bf6a895e617d0900a7
Jefferson Carlos Benedito <jefiim.ad@gmail.com>
2018-03-14 15:03:54 -0300

    add post revuon function to revuoj service

--- components/revuoj/revuojService.js
+++ components/revuoj/revuojService.js
@@ -2,11 +2,17 @@ app.service('revuojService', function ($http, $window, config) {
     var service = this;
 
     service.getRevuoj = getRevuoj;
+    service.postRevuon = postRevuon;
 
 
     function getRevuoj() {
         return $http.get(config.api_url + '/revuoj');
     };
+
+
+    function postRevuon(data) {
+        return $http.post(config.api_url + '/revuoj', data);
+    }
 
     return service;
 });
