937f656d04a9c0d29d4d6b1354bde46a24de4fac
Jefferson Carlos Benedito <jefiim.ad@gmail.com>
2018-03-15 18:11:55 -0300

    add component to post a volumon

--- components/revuoj/volumon/addVolumonCtrl.js
+++ components/revuoj/volumon/addVolumonCtrl.js
@@ -1,3 +1,23 @@
-app.controller('addRevuonVolumonCtrl', function ($scope, $window, config, $rootScope, auth, revuojService, $mdDialog) {
-    console.log("volumon controller");
+app.controller('addVolumonCtrl', function ($scope, $window, config, $rootScope, auth, revuojService, $mdDialog, errorService, revuonId) {
+    $scope.volumon = {};
+
+    $scope.addVolumon = function () {
+        $scope.volumon.enhavlisto = getTrixInputContent();
+        $scope.volumon.eldondato = new Date();
+
+        var success = function (response) {
+            $scope.volumon.id = response.data.insertId;
+            $mdDialog.hide($scope.volumon);
+        };
+
+        revuojService.addVolumon(revuonId, $scope.volumon).then(success, errorService.error);
+    };
+
+    var getTrixInputContent = function() {
+        return document.getElementById('trix').innerHTML;
+    };
+
+    $scope.cancel = function () {
+        $mdDialog.cancel();
+    };
 });
