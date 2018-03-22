b478d15e60cdab064401d6c6e3a93ab053b40584
Jefferson Carlos Benedito <jefiim.ad@gmail.com>
2018-03-22 11:19:45 -0300

    post malpeza, kovrilbido and kvalita

--- components/revuoj/volumon/addVolumonCtrl.js
+++ components/revuoj/volumon/addVolumonCtrl.js
@@ -1,4 +1,4 @@
-app.controller('addVolumonCtrl', function ($scope, $window, config, $rootScope, auth, revuojService, $mdDialog, errorService, revuonId) {
+app.controller('addVolumonCtrl', function ($scope, $window, config, $rootScope, auth, revuojService, $mdDialog, errorService, revuonId, $http) {
     $scope.volumon = {};
 
     $scope.addVolumon = function () {
@@ -8,9 +8,17 @@ app.controller('addVolumonCtrl', function ($scope, $window, config, $rootScope, 
         var success = function (response) {
             $scope.volumon.id = response.data.insertId;
             $mdDialog.hide($scope.volumon);
+
+            revuojService.postVolumonKovrilbildo(response.data.insertId, $scope.getFile('input-kovrilbildo-id'));
+            revuojService.postVolumonKvalita(response.data.insertId, $scope.getFile('input-kvalita-id'));
+            revuojService.postVolumonMalpeza(response.data.insertId, $scope.getFile('input-malpeza-id'));
         };
 
         revuojService.addVolumon(revuonId, $scope.volumon).then(success, errorService.error);
+    };
+
+    $scope.getFile = function (id) {
+        return document.getElementById(id).files[0];
     };
 
     var getTrixInputContent = function() {
